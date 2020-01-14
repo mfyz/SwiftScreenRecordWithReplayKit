3987874ef7ab3503e4525921eba494f176589c91
Mehmet Fatih Yıldız <fatih@mfyz.com>
2020-01-14 17:00:50 -0500

    Initial Commit

---
+++ SwiftScreenRecordWithReplayKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftScreenRecordWithReplayKit
+//
+//  Created by Mehmet Fatih YILDIZ on 1/14/20.
+//  Copyright © 2020 Mehmet Fatih YILDIZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
